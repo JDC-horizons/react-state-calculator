d56e304a7b2105f98217e8382daddad0c1b83806
James Canterbury <jamesdcanterbury@protonmail.com>
2024-06-20 17:09:34 +0100

    core complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,123 @@
-import "./App.css"
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [numOne, setNumOne] = useState(0);
+  const [numTwo, setNumTwo] = useState(0);
+  const [operator, setOperator] = useState("+");
+  const [result, setResult] = useState(0);
+
+  const handleClick = (id, number) => {
+    if (id === 1) {
+      if (numOne === 0) {
+        setNumOne(number);
+      } else {
+        setNumOne(`${numOne}${number}`);
+      }
+    }
+    if (id === 2) {
+      if (numTwo === 0) {
+        setNumTwo(number);
+      } else {
+        setNumTwo(`${numTwo}${number}`);
+      }
+    }
+  };
+
+  const handleClickZero = (id) => {
+    if (id === 1) {
+      if (numOne === 0) {
+        setNumOne(0);
+      } else {
+        setNumOne(`${numOne}${0}`);
+      }
+    }
+    if (id === 2) {
+      if (numTwo === 0) {
+        setNumTwo(0);
+      } else {
+        setNumTwo(`${numTwo}${0}`);
+      }
+    }
+  };
+
+  const handleClear = (id) => {
+    if (id === 1) {
+      setNumOne(0);
+    }
+    if (id === 2) {
+      setNumTwo(0);
+    }
+  };
+
+  const handleOperator = (input) => {
+    setOperator(input);
+  };
+
+  const handleResult = () => {
+    if (operator === "÷" && numTwo === 0) {
+      alert("Cannot divide by 0!");
+      setResult(0);
+      return;
+    }
+    setResult(eval(`${numOne} ${operator} ${numTwo}`));
+    handleClear(1);
+    handleClear(2);
+  };
 
   return (
     <div className="calculator">
       <div className="panel">
-        <p>0</p>
+        <p>{numOne}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          <button onClick={() => handleClick(1, 1)}>1</button>
+          <button onClick={() => handleClick(1, 2)}>2</button>
+          <button onClick={() => handleClick(1, 3)}>3</button>
+          <button onClick={() => handleClick(1, 4)}>4</button>
+          <button onClick={() => handleClick(1, 5)}>5</button>
+          <button onClick={() => handleClick(1, 6)}>6</button>
+          <button onClick={() => handleClick(1, 7)}>7</button>
+          <button onClick={() => handleClick(1, 8)}>8</button>
+          <button onClick={() => handleClick(1, 9)}>9</button>
+          <button onClick={() => handleClickZero(1)}>0</button>
+          <button onClick={() => handleClear(1)}>Clear</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>+</p>
+        <p>{operator}</p>
         <div className="numbers">
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>÷</button>
+          <button onClick={() => handleOperator("+")}>+</button>
+          <button onClick={() => handleOperator("-")}>-</button>
+          <button onClick={() => handleOperator("*")}>*</button>
+          <button onClick={() => handleOperator("÷")}>÷</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>0</p>
+        <p>{numTwo}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          <button onClick={() => handleClick(2, 1)}>1</button>
+          <button onClick={() => handleClick(2, 2)}>2</button>
+          <button onClick={() => handleClick(2, 3)}>3</button>
+          <button onClick={() => handleClick(2, 4)}>4</button>
+          <button onClick={() => handleClick(2, 5)}>5</button>
+          <button onClick={() => handleClick(2, 6)}>6</button>
+          <button onClick={() => handleClick(2, 7)}>7</button>
+          <button onClick={() => handleClick(2, 8)}>8</button>
+          <button onClick={() => handleClick(2, 9)}>9</button>
+          <button onClick={() => handleClickZero(2)}>0</button>
+          <button onClick={() => handleClear(2)}>Clear</button>
         </div>
       </div>
       <div className="panel answer">
-        <p>0</p>
+        <p>{result}</p>
         <div>
-          <button>=</button>
+          <button onClick={() => handleResult()}>=</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
